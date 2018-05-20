39c34711bb83038c6886a17367fcf54bfe7ae423
Tomás Vieira Caldas <tomas_v_caldas@hotmail.com>
2018-05-20 17:41:16 +0100

    API informaion and company picker with mvvm
    Missing passing the company name and tick to the main page

---
+++ CMOVproj2/CMOVproj2/CMOVproj2/Models/Company.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMOVproj2
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public string Tick { get; set; }
+    }
+}
---
+++ CMOVproj2/CMOVproj2/CMOVproj2/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace CMOVproj2.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public IList<Company> Companies { get { return Models.CompanyData.Companies; } }
+
+        Company selectedCompany;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Company SelectedCompany
+        {
+            get { return selectedCompany; }
+            set
+            {
+                if (selectedCompany != value)
+                {
+                    selectedCompany = value;
+                    Console.Out.WriteLine("COMPANY: {0}", selectedCompany.Name);
+                    
+                }
+            }
+        }
+
+    }
+}
